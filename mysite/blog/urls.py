772b9be4128f208dfd0322792e0e195a93557b17
shivani <shivaniaag1247@gmail.com>
2018-12-20 03:27:27 +0530

    django project

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+urlpatterns = [path('blog/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")),
+               path('blog(?P<pk>\d+)/', DetailView.as_view(
+                   model=Post,
+                   template_name="blog/post.html")),
+
+               ]
